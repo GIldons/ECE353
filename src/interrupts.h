a13418b4b4c1a02062c71a66324189af982cc3c2
Denis Medeiros <dnsricardo@gmail.com>
2015-03-21 17:03:21 -0500

    Reorganazing files

--- src/interrupts.h
+++ src/interrupts.h
@@ -6,3 +6,24 @@
 #include "TM4C123.h"
 
 #endif
+
+//*****************************************************************************
+// Used to debounce the pushbutton and check the potentiometer.
+//*****************************************************************************
+void SysTick_Handler(void);
+
+//*****************************************************************************
+// Timer0A Interrupt Handler
+// Used to time the "ON" portion of the waveform
+//*****************************************************************************
+void TIMER0A_Handler(void) ;
+
+//*****************************************************************************
+// Turns off Timer0B.  This function does not alter the load value.
+//*****************************************************************************
+void TIMER0B_Handler(void);
+
+
+
+
+
